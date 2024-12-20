c76ea68a9b52eae315ffa2366c84dfa6f1da6ee6
yinka3 <adedewe.a@northeastern.edu>
2024-12-20 13:28:51 -0500

    seperated files

--- src/models/utils.py
+++ src/models/utils.py
@@ -1,4 +1,29 @@
 import re
+from enum import Enum
+
+class UserRole(Enum):
+    BUYER = "buyer"
+    SELLER = "seller"
+    BOTH = "both"
+    NONE = "none"
+
+class ContentType(Enum):
+    POST = "post"
+    FANART = "fanart"
+    COMMENT = "comment"
+
+class OrderStatus(Enum):
+    PENDING = "pending"
+    ACCEPTED = "accepted"
+    REJECTED = "rejected"
+    INPROGRESS = "inprogress"
+    CANCELLED = "cancelled"
+    COMPLETED = "completed"
+
+class Proficiency(Enum):
+    BEGINNER = "beginner"
+    INTERMEDIATE = "intermediate"
+    EXPERT = "expert"
 
 def parse_mentions(content):
     return re.findall(r"@(\w+)", content)
