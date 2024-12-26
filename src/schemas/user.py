1fc76fa293375eaf52da1ada452599120405ae52
yinka3 <adedewe.a@northeastern.edu>
2024-12-26 12:41:51 -0500

    creating team schema

--- src/schemas/user.py
+++ src/schemas/user.py
@@ -1,12 +1,11 @@
 import re
-
+import team
 from pydantic import BaseModel, EmailStr, Field, validator, field_validator
 from typing import List, Optional
 from uuid import UUID
 from datetime import datetime
 from enum import Enum
 
-from src.models.chat import ChatMembers
 
 
 ## TODO: decide if I want to keep a limit to the password or not and where
@@ -33,6 +32,12 @@ class BuyersSchema(BuyersBaseSchema):
     class Config:
         orm_mode = True
 
+class CreateBuyerSchema(BuyersBaseSchema):
+    user_id: UUID = Field(..., description="The buyer's unique ID.")
+
+    class Config:
+        orm_mode = True
+
 class SellersBaseSchema(BaseModel):
     skills: dict = Field(..., description="Skills of the seller.")
     portfolio_url: Optional[str] = Field(None, description="URL to the seller's portfolio.")
@@ -49,44 +54,11 @@ class SellersSchema(SellersBaseSchema):
     class Config:
         orm_mode = True
 
-class CreateTeamMemberSchema(BaseModel):
-    user_id: UUID = Field(..., description="The unique identifier of the user to be added as a team member.")
-    username: str = Field(..., min_length=3, max_length=50, description="The username of the team member.")
+class CreateSellerSchema(SellersBaseSchema):
+    user_id: UUID = Field(..., description="The seller's unique ID.")
 
     class Config:
         orm_mode = True
-
-
-class CreateTeamSchema(BaseModel):
-    name: str = Field(..., min_length=3, max_length=50, description="The name of the team.")
-    members: Optional[List[CreateTeamMemberSchema]] = Field(
-        None, description="A list of user IDs and usernames to be added as initial members of the team."
-    )
-
-    class Config:
-        orm_mode = True
-
-
-class TeamSchema(CreateTeamSchema):
-    pass
-
-    class Config:
-        orm_mode = True
-
-
-class UserSchema(BaseModel):
-    id: UUID = Field(..., description="The unique identifier of the user.")
-    username: str = Field(..., description="The user's unique username.")
-    email: EmailStr = Field(..., description="The user's email address.")
-    role: Optional[UserRoleEnum] = Field(None, description="The user's role in the system.")
-    buyer_profile: Optional[BuyersSchema] = None
-    seller_profile: Optional[SellersSchema] = None
-    is_active: bool = Field(..., description="Indicates whether the user account is active.")
-    created_at: datetime = Field(..., description="The date and time when the user account was created.")
-    teams: Optional[TeamSchema] = Field(None, description="The user's teams.")
-    class Config:
-        orm_mode = True
-
 
 
 class UserBaseSchema(BaseModel):
@@ -94,7 +66,36 @@ class UserBaseSchema(BaseModel):
     email: EmailStr = Field(..., description="The user's email address.")
     role: Optional[UserRoleEnum] = Field(None, description="The user's role in the system.")
     is_active: bool = Field(..., description="Indicates whether the user is active.")
-    teams: Optional[TeamSchema] = Field(None, description="The user's teams.")
+    created_at: datetime = Field(..., description="The date and time when the user account was created.")
+
+    class Config:
+        orm_mode = True
+
+class UserProfileSchema(UserBaseSchema):
+    id: UUID = Field(..., description="The unique identifier of the user.")
+    teams: Optional[team.TeamSchema] = Field(None, description="The user's teams.")
+    buyer_profile: Optional[BuyersSchema] = Field(None, description="The user's buyer profile.")
+    seller_profile: Optional[SellersSchema] = Field(None, description="The user's seller profile.")
+
+    class Config:
+        orm_mode = True
+
+
+class UserSchema(UserProfileSchema):
+    is_active: bool = Field(..., description="Indicates whether the user account is active.")
+
+    class Config:
+        orm_mode = True
+
+
+class UpdateUserSchema(UserBaseSchema):
+    pass
+
+    class Config:
+        orm_mode = True
+
+class DisplayUserSummarySchema(UserBaseSchema):
+    id: UUID = Field(..., description="The unique identifier of the user.")
 
     class Config:
         orm_mode = True
@@ -123,13 +124,11 @@ class CreateUserSchema(UserBaseSchema):
     class Config:
         orm_mode = True
 
-
-
-class CreateBuyerSchema(BuyersBaseSchema):
-    user_id: UUID = Field(..., description="The buyer's unique ID.")
+class DeleteUserSchema(BaseModel):
+    id: UUID = Field(..., description="The unique identifier of the user.")
+    reason: Optional[str] = Field(None, description="Reason for deleting the user account.")
 
     class Config:
         orm_mode = True
-
 
 
