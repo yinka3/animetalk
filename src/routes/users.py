f7e6550774eb8fd30cf467f117f9c0a7539a7cf3
yinka3 <adedewe.a@northeastern.edu>
2025-01-07 14:58:27 -0500

    working on login precess

--- src/routes/users.py
+++ src/routes/users.py
@@ -4,12 +4,13 @@ from typing import Optional
 
 import jwt
 import uvicorn
-from fastapi import FastAPI, HTTPException, Depends
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import FastAPI, HTTPException, Depends, status, Response
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jwt import PyJWTError
 from pydantic import BaseModel
-from pydantic.v1 import BaseSettings
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, InstrumentedAttribute
+
+from src import utils
 from src.schemas.user import CreateUserSchema, UserSchema, CreateUserResponseSchema, UserPasswordUpdateResponse, UserPasswordUpdate, GetUserResponseSchema, UserProfileSchema
 from src.models.roles import Users, Teams
 from uuid import UUID
@@ -19,11 +20,14 @@ from src.utils import hash_password, verify_password
 app = FastAPI()
 
 class Token(BaseModel):
-    access_token: str
+    access_token: InstrumentedAttribute[str]
     token_type: str
+    token_expires: Optional[datetime]
 
 class TokenData(BaseModel):
-    id: Optional[UUID] = None
+    user: Optional[str] = None
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
 @app.post("/users/register", response_model=CreateUserResponseSchema)
 def create_user(user: CreateUserSchema, db: Session = Depends(get_db)):
@@ -48,21 +52,17 @@ def create_access_token(data: dict):
     return encoded_jwt
 
 # might need to have a param: form_data: OAuth2PasswordRequestForm = Depends()
-def get_access_token(user_id: UUID):
+def get_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
     # access_token_expires = timedelta(minutes=int(os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES")))
     access_token = create_access_token(
-        data={"user_id": user_id},
+        data={"sub": form_data.username},
     )
     return access_token
 
 
-def authenticate_user(token: UUID):
+def authenticate_user(token: str):
     try:
         payload = jwt.decode(token, os.environ.get("SECRET_KEY"), algorithms=[os.environ.get("ALGORITHM")])
-        user_id: UUID = payload.get("user_id")
-        if not user_id:
-            raise HTTPException(status_code=400, detail="Incorrect user")
-        token_data = TokenData(id=user_id)
 
         exp_time = payload.get("exp")
         if exp_time is None or exp_time < datetime.now().timestamp():
@@ -70,15 +70,44 @@ def authenticate_user(token: UUID):
             # recreating this code on top w/o entity manager
             raise HTTPException(status_code=401, detail="Authentication failed, invalid or expired token.")
 
+        username: str = payload.get("sub")
+        if not username:
+            raise HTTPException(status_code=400, detail="Incorrect user")
+        token_data = TokenData(user=username)
+        return token_data
+
         # Check if the username exists in the active sessions
         # session = self.activeSessionsEntityManager.find_one({"username": username})
         # if not session:
-        #     aise HTTPException(status_code=401, detail="User session not found.")
-
+        #     raise HTTPException(status_code=401, detail="User session not found.")
     except PyJWTError:
         raise HTTPException(status_code=401, detail="Authentication failed, invalid or expired token.")
 
-    return token_data
+@app.post("/user/login", response_model=Token)
+def login(response: Response, form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(Users).filter(Users.email == form_data.username).first()
+    if not user or not utils.verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Invalid credentials.",
+        )
+    access_token = create_access_token(data={"sub": user.username})
+    response.set_cookie(key="access_token", value=access_token, httponly=True, secure=True)
+
+    return {"message": "Welcome Mother*ucker"}
+    #return {"access_token": access_token, "token_type": "bearer"}
+
+def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+
+    token = authenticate_user(token)
+
+    if not token:
+        raise HTTPException(status_code=401, detail="Authentication failed, invalid or expired token.")
+    user = db.query(Users).filter(Users.username == token.username).first()
+
+    if not user:
+        raise HTTPException(status_code=401, detail="Authentication failed, invalid or expired token.")
+    return user
 
 @app.get("users/me", response_model=UserProfileSchema)
 def get_my_account(current_user: Users = Depends(get_current_user)):
@@ -98,6 +127,7 @@ def get_user_by_id(user_id: UUID, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="User not found.")
 
     return user
+
 
 @app.get("/users/{username}", response_model=GetUserResponseSchema)
 def get_user_by_username(username: UUID, db: Session = Depends(get_db)):
